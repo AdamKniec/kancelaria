f03d57c28207315aeb2d8905875d9b9d76438d59
AdamKniec <adam.k.kniec@gmail.com>
2018-10-31 22:25:44 +0100

    adding owl carousel and header markup + styles

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert('test');
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})

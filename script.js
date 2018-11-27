c1bb6dfa9be7237d741f2fdd7ed9621db94f25df
AdamKniec <adam.k.kniec@gmail.com>
2018-11-27 21:46:19 +0100

    paths changes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-    items:3,
+    // items:3,
     loop:true,
     margin:10,
     autoplay:true,
     autoplayTimeout:5000,
-    autoplayHoverPause:true
+    autoplayHoverPause:true,  
 });
 $('.play').on('click',function(){
     owl.trigger('play.owl.autoplay',[1000])

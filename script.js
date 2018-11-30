4137f88f8752166343bf539ce8a8186e99435209
AdamKniec <adam.k.kniec@gmail.com>
2018-11-30 15:51:17 +0100

    adding css styling changes and new html markup

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    // items:3,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:5000,
-    autoplayHoverPause:true,  
-});
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
+//OwlGallery handler
+(function(){
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        // items:3,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,  
+    });
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[1000])
+    })
+    $('.stop').on('click',function(){
+        owl.trigger('stop.owl.autoplay')
+    })  
+}());
+//StickyNavbar
+(function(){
+    const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', ()=> {
+        if(window.pageYOffset > 0) {
+            navbar.classList.add('sticky');
+        } else {
+            navbar.classList.remove('sticky');
+        }
+    })
+}());
+

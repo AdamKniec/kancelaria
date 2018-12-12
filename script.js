946a03629f6050818d48762238149b70a477a917
AdamKniec <adam.k.kniec@gmail.com>
2018-12-12 23:19:03 +0100

    css and js changes

--- script.js
+++ script.js
@@ -1,39 +1,4 @@
-//OwlGallery handler
-(function(){
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
-        // items:2,
-        loop:true,
-        margin:10,
-        autoplay:true,
-        autoplayTimeout:5000,
-        autoplayHoverPause:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:2,
-                nav:false
-            },
-            1000:{
-                items:3,
-                nav:true,
-                loop:true
-            }
-        }
-        
-    });
-    // $('.play').on('click',function(){
-    //     owl.trigger('play.owl.autoplay',[1000])
-    // })
-    // $('.stop').on('click',function(){
-    //     owl.trigger('stop.owl.autoplay')
-    // })  
-    
-}());
+
 //StickyNavbar
 (function(){
     const navbar = document.querySelector('.navbar');
@@ -45,4 +10,33 @@
         }
     })
 }());
+//Modal handler
+(function(){
+    let modal = document.querySelector('.modal');
+    let termsTrigger = document.querySelector('.termsTrigger');
+    let closeButton = document.querySelector('.closeButton');
+    let form = document.querySelector('form');
+        form.addEventListener('click', (e)=> {
+            if(e.target == termsTrigger) {
+                modal.style.display = "flex";
+            } else if (e.target == modal || e.target == closeButton)  {
+                modal.style.display = "none";
+            }
+        })
+}());
 
+//FORM SUBMISSION HANDLER
+(function(){
+    let checkbox = document.querySelector('.termsAgreement');
+    let form = document.querySelector('form');
+    let agreementParagraph = document.querySelector('.agreement p');
+    
+    form.addEventListener('submit' , (e) => {
+        e.preventDefault();
+        if(checkbox.checked == true) {
+            form.submit();
+        } else {
+            agreementParagraph.style.color = "red";
+        }
+    })
+}());

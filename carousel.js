b78786fe8fd97ea04c81dd73bd174afcc53ca506
AdamKniec <adam.k.kniec@gmail.com>
2018-12-19 18:25:02 +0100

    changing css styles

--- carousel.js
+++ carousel.js
@@ -27,11 +27,4 @@
         }
         
     });
-    // $('.play').on('click',function(){
-    //     owl.trigger('play.owl.autoplay',[1000])
-    // })
-    // $('.stop').on('click',function(){
-    //     owl.trigger('stop.owl.autoplay')
-    // })  
-    
 }());

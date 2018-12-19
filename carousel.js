f4124cf62871bfecdda3dea0d5a641fec49850d7
AdamKniec <adam.k.kniec@gmail.com>
2018-12-20 00:13:51 +0100

    css changes and second carousel addition

--- carousel.js
+++ carousel.js
@@ -15,6 +15,36 @@
                 items:1,
                 nav:true
             },
+            768:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:2,
+                nav:true,
+                loop:true
+            }
+        }
+        
+    });
+}());
+
+//OwlGallery handler
+(function(){
+    var owl = $('.owl-team owl-carousel');
+    owl.owlCarousel({
+        items:2,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
             600:{
                 items:2,
                 nav:false
